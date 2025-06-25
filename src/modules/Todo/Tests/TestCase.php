764b5e8df8db31ebe4d75d41059e5444908f898c
Lawal Rashidi <lawalrashidi95@gmail.com>
2025-06-25 22:03:38 +0100

    adjustment

--- src/modules/Todo/Tests/TestCase.php
+++ src/modules/Todo/Tests/TestCase.php
@@ -3,10 +3,11 @@
 declare(strict_types=1);
 namespace Modules\Todo\Tests;
 
+use \PHPUnit\Framework\TestCase as BaseTestCase;
 // use BitCore\Tests\TestCase as BaseTestCase;
 
 
-use Tests\TestCase as BaseTestCase;
+// use Tests\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -26,4 +26,6 @@ return array(
     '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
     'a4a119a56e50fbb293281d9a48007e0e' => $vendorDir . '/symfony/polyfill-php80/bootstrap.php',
     'ef65a1626449d89d0811cf9befce46f0' => $vendorDir . '/illuminate/events/functions.php',
+    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+    'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
 );
