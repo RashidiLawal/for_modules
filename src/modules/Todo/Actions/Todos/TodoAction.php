a395fd660850f06ca4c05034015d28ddd6362ca7
Lawal Rashidi <lawalrashidi95@gmail.com>
2025-06-30 14:16:18 +0100

    improved

--- src/modules/Todo/Actions/Todos/TodoAction.php
+++ src/modules/Todo/Actions/Todos/TodoAction.php
@@ -6,7 +6,12 @@ namespace Modules\Todo\Actions\Todos;
 
 use BitCore\Application\Actions\Action;
 use Modules\Todo\Repositories\Todos\TodoRepositoryInterface;
-
+/**
+ * Class TodoAction
+ *
+ * The Base Action for all Todo related actions.
+ * It initializes the TodoRepositoryInterface for use in derived classes.
+ */
 abstract class TodoAction extends Action
 {
     protected TodoRepositoryInterface $todoRepository;
